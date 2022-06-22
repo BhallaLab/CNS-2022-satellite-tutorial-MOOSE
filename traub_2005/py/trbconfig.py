e480aeadebd484d5792a6630c8f11bd23e415351
hrani <ranishashi@gmail.com>
2022-06-22 15:59:45 +0530

    Files

---
+++ traub_2005/py/trbconfig.py
@@ -0,0 +1,155 @@
+# config.py --- 
+# 
+# Filename: config.py
+# Description: 
+# Author: Subhasis Ray
+# Maintainer: 
+# Created: Fri May  4 14:46:29 2012 (+0530)
+# Version: 
+# Last-Updated: Fri May  4 21:05:04 2012 (+0530)
+#           By: Subhasis Ray
+#     Update #: 140
+# URL: 
+# Keywords: 
+# Compatibility: 
+# 
+# 
+
+# Commentary: 
+# 
+# 
+# 
+# 
+
+# Change log:
+# 
+# 
+# 
+
+# Code:
+
+from datetime import datetime
+try:
+    import configparser as configparser
+except Exception as e:
+    import ConfigParser  as configparser
+import logging
+import numpy
+import os
+import moose
+
+# runtime info
+timestamp = datetime.now()
+mypid = os.getpid()
+# Unit Conversion Factors
+uS = 1e-6 # micro Siemens to Siemens
+ms = 1e-3 # milli second to second
+mV = 1e-3 # milli Volt to Volt
+
+# limits on HH-gate tables
+vmin = -120 * mV
+vmax = 40 * mV
+ndivs = 640
+dv = (vmax - vmin)/ndivs
+
+# element to contain prototypes
+libpath = '/library'
+
+# defined channels to be initialized in prototypes
+channel_names = ['AR',
+                 'CaPool',
+                 'CaL',
+                 'CaT',
+                 'CaT_A',
+                 'K2',
+                 'KA',
+                 'KA_IB',
+                 'KAHP',
+                 'KAHP_DP',
+                 'KAHP_SLOWER',
+                 'KC',
+                 'KC_FAST',
+                 'KDR',
+                 'KDR_FS',
+                 'KM',
+                 'NaF',
+                 'NaF2',
+                 'NaF_TCR',
+                 'NaP',
+                 'NaPF',
+                 'NaPF_SS',
+                 'NaPF_TCR',
+                 'NaF2_nRT']
+
+############################################
+# Parse configuration file
+############################################
+sdir = os.path.dirname( __file__ )
+_parser = configparser.ConfigParser()
+_parser.optionxform = str
+_parser.read([os.path.join(sdir,'defaults.ini'), os.path.join(sdir,'custom.ini')])
+
+# seed for random number generator in MOOSE
+moose_rngseed = 10 #_parser.get('numeric', 'moose_rngseed')
+
+# seed for random number generator in numpy
+numpy_rngseed = 12 # _parser.get('numeric', 'numpy_rngseed')
+# flag if the simulation uses stochastic synchans
+stochastic = _parser.get('numeric', 'stochastic') in ['Yes', 'yes', 'True', 'true', '1']
+reseed = _parser.get('numeric', 'reseed') in ['Yes', 'yes', 'True', 'true', '1']
+solver = _parser.get('numeric', 'solver')
+simtime = float(_parser.get('scheduling', 'simtime'))
+simdt = float(_parser.get('scheduling', 'simdt'))
+plotdt = float(_parser.get('scheduling', 'plotdt'))
+
+######################################################################
+# configuration for saving simulation data
+######################################################################
+datadir = '/tmp'
+if not os.access(datadir, os.F_OK):
+    os.makedirs(datadir)
+rotodir = '/tmp'
+datafileprefix = 'data'
+netfileprefix = 'network'
+filesuffix = '_%s_%d' % (timestamp.strftime('%Y%m%d_%H%M%S'), mypid)
+datafilepath = os.path.join(datadir, datafileprefix + filesuffix + '.h5')
+netfilepath = os.path.join(datadir, netfileprefix + filesuffix + '.h5')
+
+#####################################################################
+#  Logging
+#####################################################################
+logfileprefix = 'traub2005'
+logfilename = os.path.join(datadir, logfileprefix + filesuffix + '.log')
+loglevel = 1
+logger = logging.getLogger(logfileprefix)
+logging.basicConfig(filename=logfilename,
+                    level=loglevel,
+                    format='%(asctime)s \
+                    %(levelname)s \
+                    %(name)s \
+                    %(filename)s \
+                    %(funcName)s: \
+                    %(message)s',
+                    filemode='w')
+benchmark = 1
+benchmarker = logging.getLogger(logfileprefix + '.benchmark')
+benchmarker.setLevel(logging.DEBUG)
+
+_inited = False
+def init():
+    global _inited
+    if _inited:
+        return
+    _inited = True
+    if reseed:
+        if moose_rngseed:
+            moose.seed(int(moose_rngseed))
+        else:
+            moose.seed(0)
+        if numpy_rngseed:
+            numpy.random.seed(int(numpy_rngseed))
+
+init()
+
+# 
+# config.py ends here
---
+++ tutorials/ChemicalBistables/doseResponse.py
@@ -0,0 +1,127 @@
+## Makes and plots the dose response curve for bistable models
+## Author: Sahil Moza
+## June 26, 2014
+
+import moose
+import numpy as np
+from matplotlib import pyplot as plt
+
+def setupSteadyState(simdt,plotDt):
+
+    stoich = moose.Stoich( '/model/kinetics/stoich' )
+    ksolve = moose.Ksolve( '/model/kinetics/ksolve' )
+    stoich.compartment = moose.element('/model/kinetics')
+
+    stoich.ksolve = ksolve
+    #ksolve.stoich = stoich
+    stoich.reacSystemPath = "/model/kinetics/##"
+    state = moose.SteadyState( '/model/kinetics/state' )
+   
+    #### Set clocks here
+    #moose.useClock(4, "/model/kinetics/##[]", "process")
+    #moose.setClock(4, float(simdt))
+    #moose.setClock(5, float(simdt))
+    #moose.useClock(5, '/model/kinetics/ksolve', 'process' )
+    #moose.useClock(8, '/model/graphs/#', 'process' )
+    #moose.setClock(8, float(plotDt))
+ 
+    moose.reinit()
+
+    state.stoich = stoich
+    state.showMatrices()
+    state.convergenceCriterion = 1e-8
+    
+    return ksolve, state
+
+def parseModelName(fileName):
+    pos1=fileName.rfind('/')
+    pos2=fileName.rfind('.')
+    directory=fileName[:pos1]
+    prefix=fileName[pos1+1:pos2]
+    suffix=fileName[pos2+1:len(fileName)]
+    return directory, prefix, suffix
+
+# Solve for the steady state
+def getState( ksolve, state, vol):
+      scale = 1.0 / ( vol * 6.022e23 )
+      moose.reinit()
+      state.randomInit() # Removing random initial condition to systematically make Dose reponse curves.
+      moose.start( 2.0 ) # Run the model for 2 seconds.
+      state.settle()
+      
+      vector = []
+      a = moose.element( '/model/kinetics/a' ).conc
+      #print a
+      for x in ksolve.nVec[0]:
+          vector.append( x * scale)
+      moose.start( 10.0 ) # Run model for 10 seconds, just for display
+      failedSteadyState = any([np.isnan(x) for x in vector])
+      
+      if not (failedSteadyState):
+           return state.stateType, state.solutionStatus, a, vector
+
+
+def main():
+    # Setup parameters for simulation and plotting
+    simdt= 1e-2
+    plotDt= 1
+
+    # Factors to change in the dose concentration in log scale
+    factorExponent = 10  ## Base: ten raised to some power.
+    factorBegin = -20
+    factorEnd = 21
+    factorStepsize = 1
+    factorScale = 10.0 ## To scale up or down the factors
+
+    # Load Model and set up the steady state solver.
+    # model = sys.argv[1] # To load model from a file.
+    model = './19085.cspace'
+    modelPath, modelName, modelType = parseModelName(model)
+    outputDir = modelPath
+    
+    modelId = moose.loadModel(model, 'model', 'ee')
+    dosePath = '/model/kinetics/b/DabX' # The dose entity
+
+    ksolve, state = setupSteadyState( simdt, plotDt)
+    vol = moose.element( '/model/kinetics' ).volume
+    iterInit = 100
+    solutionVector = []
+    factorArr = []
+    
+    enz = moose.element(dosePath)
+    init = float(enz.kcat) # Dose parameter
+    
+    # Change Dose here to .
+    for factor in range(factorBegin, factorEnd, factorStepsize ):
+        scale = factorExponent ** (factor/factorScale) 
+        enz.kcat = init * scale     
+        print( "scale={:.3f}\tkcat={:.3f}".format( scale, enz.kcat) )
+        for num in range(iterInit):
+            stateType, solStatus, a, vector = getState( ksolve, state, vol)
+            if solStatus == 0:
+                #solutionVector.append(vector[0]/sum(vector))
+                solutionVector.append(a)
+                factorArr.append(scale)   
+                
+    joint = np.array([factorArr, solutionVector])
+    joint = joint[:,joint[1,:].argsort()]
+    
+    # Plot dose response. Remove NaN from the values else plotting will fail.
+    ax = plt.subplot()
+    # plt.semilogx was failing. not sure why. That is why this convoluted
+    # approach.
+    ax.plot( joint[0,:], joint[1,:] , marker="o", label = 'concA')
+    ax.set_xscale( 'log' )
+    plt.xlabel('Dose')
+    plt.ylabel('Response')
+    plt.suptitle('Dose-Reponse Curve for a bistable system')
+    
+    plt.legend(loc=3)
+    #plt.savefig(outputDir + "/" + modelName +"_doseResponse" + ".png")
+    plt.show()
+    quit()
+    
+
+
+if __name__ == '__main__':
+     main()
